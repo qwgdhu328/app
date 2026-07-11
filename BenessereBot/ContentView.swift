d73ceec67e7b0535796a89ad75a045fe919cf84a
qwgdhu328 <angelo.cremina16@gmail.com>
2026-07-11 20:25:16 +0200

    Fix: revert TabView to traditional .tag()/.tabItem() API (Tab init non supportata in questo SDK)

--- BenessereBot/ContentView.swift
+++ BenessereBot/ContentView.swift
@@ -5,18 +5,18 @@ struct ContentView: View {
 
     var body: some View {
         TabView(selection: $selectedTab) {
-            Tab(Tab.home.rawValue, systemImage: Tab.home.icon) {
-                HomeView()
-            }
-            Tab(Tab.chat.rawValue, systemImage: Tab.chat.icon) {
-                ChatView()
-            }
-            Tab(Tab.community.rawValue, systemImage: Tab.community.icon) {
-                CommunityView()
-            }
-            Tab(Tab.profile.rawValue, systemImage: Tab.profile.icon) {
-                ProfileView()
-            }
+            HomeView()
+                .tag(Tab.home)
+                .tabItem { Label(Tab.home.rawValue, systemImage: Tab.home.icon) }
+            ChatView()
+                .tag(Tab.chat)
+                .tabItem { Label(Tab.chat.rawValue, systemImage: Tab.chat.icon) }
+            CommunityView()
+                .tag(Tab.community)
+                .tabItem { Label(Tab.community.rawValue, systemImage: Tab.community.icon) }
+            ProfileView()
+                .tag(Tab.profile)
+                .tabItem { Label(Tab.profile.rawValue, systemImage: Tab.profile.icon) }
         }
         .tabViewStyle(.tabBarOnly)
     }
